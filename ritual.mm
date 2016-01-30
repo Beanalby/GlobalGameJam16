10a6f2e599b05de0efbebb58c1844b02beceda3f
Jason Viers <bean@beanalby.net>
2016-01-30 10:54:59 -0500

    First commit with mindmap

---
+++ ritual.mm
@@ -0,0 +1,12 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1454113848923" ID="ID_1703430027" MODIFIED="1454117479980" TEXT="ritual">
+<node CREATED="1454113871846" ID="ID_164902316" MODIFIED="1454117521437" POSITION="right" TEXT="actions without consequence, eventual effect">
+<node CREATED="1454117511708" ID="ID_12710532" MODIFIED="1454117536202" TEXT="hit metal with hammer, tempers shield"/>
+<node CREATED="1454117537144" ID="ID_1405197975" MODIFIED="1454117547834" TEXT="place out food, feeds person/wolves?"/>
+<node CREATED="1454117548791" ID="ID_1374292392" MODIFIED="1454117702264" TEXT="Sweep floor, ???"/>
+<node CREATED="1454118010015" ID="ID_1335584164" MODIFIED="1454118153883" TEXT="Pray, becomes weapon"/>
+<node CREATED="1454119112564" ID="ID_1016280532" MODIFIED="1454119120210" TEXT="Stir water, maintains purity"/>
+</node>
+</node>
+</map>
