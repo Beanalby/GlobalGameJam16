84a19bd157968ebd542c665ffc93fd518f9d6111
Jason Viers <bean@beanalby.net>
2016-01-30 13:25:25 -0500

    Added wall, floor, basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GlobalGameJam16 {
+    public class Player: MonoBehaviour {
+        private float moveSpeed = 3f;
+
+        Rigidbody2D rb;
+
+        public void Start() {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        public void Update() {
+            rb.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), moveSpeed * Input.GetAxis("Vertical"));
+        }
+    }
+}
