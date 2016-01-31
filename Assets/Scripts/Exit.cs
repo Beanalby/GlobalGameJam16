b39b67a7905b8934dc642472312e460e5067bd70
Jason Viers <bean@beanalby.net>
2016-01-30 20:01:25 -0500

    Added bed and exit sunlight

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+    public void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("+++ exiing");
+    }
+}
