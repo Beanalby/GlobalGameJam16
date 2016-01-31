91a80daa853ab9b6084d01a41d8a822348c045f8
Jason Viers <bean@beanalby.net>
2016-01-30 19:19:13 -0500

    Expanded room, added camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject target;
+    float maxPosition;
+    float minPosition;
+
+    public void FixedUpdate() {
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, target.transform.position.x, 0.02f),
+            transform.position.y,
+            transform.position.z);
+    }
+}
