4a915fc5c15b6242a0bb921365e5c9259c8b3e74
Jason Viers <bean@beanalby.net>
2016-01-31 11:00:13 -0500

    Fleshed out end day badness detection

--- Assets/Scripts/EndDay.cs
+++ Assets/Scripts/EndDay.cs
@@ -5,12 +5,28 @@ using System.Collections;
 namespace GlobalGameJam16 {
     public class EndDay: MonoBehaviour {
         public Text endLabel;
+        public Image badImage;
+        private float threshold = .666f;
 
         public void Start() {
             GameState state = GameState.Instance;
-            string dirtStat = state.NumDirtFailed + "/" + (state.NumDirtFailed + state.NumDirtSucceeded);
-            string prayStat = state.NumPrayFailed + "/" + (state.NumPrayFailed + state.NumPraySucceeded);
-            endLabel.text = "You cleaned " + dirtStat + " dirt and prayed " + prayStat + " scrolls.";
+            bool dirtFailed = (state.NumDirtSucceeded / (state.NumDirtFailed + state.NumDirtSucceeded)) < threshold;
+            bool prayFailed = (state.NumPraySucceeded / (state.NumPrayFailed + state.NumPraySucceeded)) < threshold;
+
+            string labelText = "";
+            if (dirtFailed) {
+                labelText += "Your negligence in cleaning the temple has made Tehlu angry.\n\n";
+            }
+            if (prayFailed) {
+                labelText += "Skipping your prayers has left Tehlu weak and abandoned.\n\n";
+            }
+            if (prayFailed || dirtFailed) {
+                labelText += "The world falls into ruins.";
+                badImage.gameObject.SetActive(true);
+            } else {
+                labelText += "Your dilligence has delivered salvation to the world.";
+            }
+            endLabel.text = labelText;
         }
     }
 }
